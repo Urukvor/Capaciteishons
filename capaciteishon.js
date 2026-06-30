d4455648738c06677f7e31dab34a67a9ba47e7a4
Uruk <meskuik@gmail.com>
2026-06-30 15:36:38 -0600

    Update capaciteishon.js

--- capaciteishon.js
+++ capaciteishon.js
@@ -1,14 +1,6 @@
-console.log("capaciteishon.js cargado acorrectamente");
-
-
-// =================================
-// EFECTO TABLAS
-// =================================
-
+//efectoTablas
 $(document).on('knack-view-render.view_4039 knack-view-render.view_4035', function() {
-
   $('#view_4039 tbody tr, #view_4035 tbody tr').each(function(index) {
-
     $(this)
       .css({
         opacity: 0,
@@ -18,43 +10,23 @@ $(document).on('knack-view-render.view_4039 knack-view-render.view_4035', functi
       .animate({
         opacity: 1
       }, 300);
-
-
+    
     setTimeout(() => {
 
       $(this).css({
         transform: 'translateY(0)',
         transition: 'all 0.3s ease'
       });
-
     }, index * 50);
-
-
   });
-
 });
 
-
-
-// =================================
-// FUNCIÓN VIEW 4034
-// =================================
-
+//view_4034
 function cargarView4034() {
-
-
   const view = $('#view_4034');
-
-
-  // si todavía no existe, salir
+  //si todavía no existe, salir
   if (!view.length) return;
-
-
-
-  // -----------------------------
   // efecto formulario
-  // -----------------------------
-
   view
     .css({
       opacity: 0,
@@ -64,93 +36,43 @@ function cargarView4034() {
       opacity: 1
     }, 400);
 
-
-
   setTimeout(function() {
 
     view.css({
       transform: 'translateY(0)',
       transition: 'all .4s ease'
     });
-
-
   },50);
-
-
-
-
-  // -----------------------------
-  // botón Examinar
-  // -----------------------------
-
+ 
+  // botónExaminar
   const input = view.find('#field_3158_upload');
-
-
-  // evita duplicados
+  // evitaDuplicados
   if (!input.length || view.find('.custom-upload-btn').length) {
     return;
   }
-
-
-
-  // oculta input original
+  // ocultaInputOriginal
   input.hide();
 
-
-
-  // crea botón personalizado
+  //creaBotónPersonalizado
   input.after(`
-
     <label for="field_3158_upload" class="custom-upload-btn">
         Examinar...
     </label>
-
   `);
-
-
-
   // detecta archivo seleccionado
   input.on('change', function() {
-
-
     const fileName = this.files && this.files.length
-
       ? this.files[0].name
-
       : 'Sin archivo seleccionado';
-
-
-
     view.find('.custom-file-name').text(fileName);
-
-
   });
-
-
-
 }
 
-
-
-// =================================
-// EVENTO NORMAL KNACK
-// =================================
-
+//eventoNormalKnack
 $(document).on('knack-view-render.view_4034', function() {
-
   cargarView4034();
-
 });
-
-
-
-
-// =================================
-// SI EL SCRIPT CARGÓ DESPUÉS
-// =================================
-
+//siElScriptCargoDespués
 $(function(){
-
   cargarView4034();
-
 });
